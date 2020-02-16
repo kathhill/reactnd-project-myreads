c99b255832bc2a2aef6403e5c4975bc4715010a5
Kathy Hill <kath.hill@sbcglobal.net>
2020-02-16 07:41:33 -0800

    refactor: Change Bookshelf to one word

--- src/App.js
+++ src/App.js
@@ -52,12 +52,12 @@ const Header = props => {
 const Bookcase = () => {
 	return (
 		<div>
-			<BookShelf />
+			<Bookshelf />
 		</div>
 	);
 };
 
-const BookShelf = () => {
+const Bookshelf = () => {
 	return (
 		<div className='bookshelf'>
 			<h2 className='bookshelf-title'>Currently Reading</h2>
@@ -84,7 +84,7 @@ const Book = props => {
 								'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
 						}}
 					></div>
-					<BookShelfChanger />
+					<BookshelfChanger />
 				</div>
 				<div className='book-title'>To Kill a Mockingbird</div>
 				<div className='book-authors'>Harper Lee</div>
@@ -93,7 +93,7 @@ const Book = props => {
 	);
 };
 
-class BookShelfChanger extends Component {
+class BookshelfChanger extends Component {
 	render() {
 		return (
 			<div className='book-shelf-changer'>
