459e993ef8bb9652bc3f7f1eb5a91ac06d0aa03e
Kathy Hill <kath.hill@sbcglobal.net>
2020-02-17 13:34:28 -0800

    chore: Create a components directory, move files and update references

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
+// import * as BooksAPI from './utils/BooksAPI';
 import './App.css';
 
 class BooksApp extends Component {
 	state = {};
 
+	// componentDidMount = () => {
+	// 	BooksAPI.getAll().then(books => {
+	// 		this.setState(() => ({
+	// 			books
+	// 		}));
+	// 	});
+	// };
+
 	render() {
 		return (
 			<div className='app'>
-				<Route path='/search-books' component={SearchBooks} />
 				<Route exact path='/' component={ListBooks} />
+				<Route path='/search-books' component={SearchBooks} />
 			</div>
 		);
 	}
