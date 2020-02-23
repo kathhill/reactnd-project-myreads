710a5606d5554102c366869fae4b13bfdcb01d9e
Kathy Hill <kath.hill@sbcglobal.net>
2020-02-23 11:58:24 -0800

    feat: Render book thumbnails, titles, and authors

--- src/components/Bookcase.js
+++ src/components/Bookcase.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import BookshelfChanger from './BookshelfChanger';
 import OpenSearchBtn from './OpenSearchBtn';
 
 class Bookcase extends Component {
@@ -9,7 +10,23 @@ class Bookcase extends Component {
 				<Header pageTitle='MyReads' />
 				<ul className='list-books-content'>
 					{this.props.books.map(book => (
-						<li key={book.id}>{book.title}</li>
+						<li key={book.id}>
+							<div className='book'>
+								<div className='book-top'>
+									<div
+										className='book-cover'
+										style={{
+											width: 128,
+											height: 193,
+											backgroundImage: `url(${book.imageLinks.thumbnail})`
+										}}
+									></div>
+									<BookshelfChanger />
+								</div>
+								<div className='book-title'>{book.title}</div>
+								<div className='book-authors'>{book.authors}</div>
+							</div>
+						</li>
 					))}
 				</ul>
 				<OpenSearchBtn />
